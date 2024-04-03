25c9220cdf378d95eb9156d81da2eb9172fde50f
Anton <antonkalash037@mail.ru>
2024-04-03 16:53:39 +0300

    homework commit

--- Flask_Master.py
+++ Flask_Master.py
@@ -10,7 +10,16 @@ def contacts():
     developer_name = "Ilon Mask"
     developer_address = "ул. Московская, д.2, кв.103"
     developer_phone = "88005553535"
-    return render_template('contacts.html', name = developer_name, phone = developer_phone, address = developer_address)
+    developer_email = "devemail@mail.ru"
+    return render_template('contacts.html', name = developer_name, phone = developer_phone, address = developer_address, email = developer_email)
+
+@app.route("/projects/")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/news/")
+def news():
+    return render_template('news.html')
 
 if __name__ == "__main__":
     app.run(debug= True)
